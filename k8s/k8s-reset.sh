f0c465feac9bc09c555d0a99c67201a8fbd59e9e
gongli.qiu <gongli.qiu@dsky.com>
2022-01-20 14:00:46 +0800

    重置k8s脚本

---
+++ k8s/k8s-reset.sh
@@ -0,0 +1,33 @@
+
+echo "-----------reset------------"
+kubeadm reset -f
+echo "-----------rmfile------------"
+rm -rf  $HOME/.config
+rm -rf  /etc/kubernetes/*
+rm -rf /etc/cni/net.d
+echo "-----------reset-net------------"
+ifconfig cni0 down
+ip link delete cni0
+
+
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+
+
+iptables -t nat -P PREROUTING ACCEPT
+iptables -t nat -P POSTROUTING ACCEPT
+iptables -t nat -P OUTPUT ACCEPT
+
+iptables -t mangle -P PREROUTING ACCEPT
+iptables -t mangle -P OUTPUT ACCEPT
+
+iptables -F
+iptables -t nat -F
+iptables -t mangle -F
+
+iptables -X
+iptables -t nat -X
+iptables -t mangle -X
+
+echo "-----------end------------"
