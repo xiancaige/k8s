bf9ffdcdc8592ff40008237a35c6c8aadccb7a42
xcg <357688537@qq.com>
2022-03-17 20:39:26 +0800

    去掉阿里云镜像验证

--- k8s/k8s-node.sh
+++ k8s/k8s-node.sh
@@ -136,9 +136,8 @@ cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 name=Kubernetes
 baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
 enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+gpgcheck=0
+repo_gpgcheck=0
 EOF
 
 
